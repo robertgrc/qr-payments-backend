4fcc31b840889757aa7fe1ea6e118b33d031af6c
robertgrc <robert.rodriguez.rgrc@gmail.com>
2023-08-31 10:44:47 -0400

    Encriptando la contrasena

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { response } = require("express");
+const bcrypt = require("bcryptjs");
 const Usuario = require("../models/Usuario");
 
 const crearUsuario = async (req, res = response) => {
@@ -14,6 +15,11 @@ const crearUsuario = async (req, res = response) => {
       });
     }
     usuario = new Usuario(req.body);
+
+    //Encriptar contrasena
+    const salt = bcrypt.genSaltSync();
+    usuario.password = bcrypt.hashSync(password, salt);
+
     await usuario.save();
     res.status(201).json({
       ok: true,
